d9b79f32a8eec73f732bd70ea6a742ed78eecc70
Ash Lux <ashlux@gmail.com>
2009-04-11 21:53:04 -0500

    Initial commit for twitarr.

---
+++ twitarr-service/src/main/java/com/ashlux/twitarr/writer/PotbsWriter.java
@@ -0,0 +1,9 @@
+package com.ashlux.twitarr.writer;
+
+import com.ashlux.potbs4j.vo.ServerDocument;
+
+public interface PotbsWriter
+{
+    public void publishServerStatusChange( ServerDocument.Server server )
+        throws TwitarrWriterException;
+}
